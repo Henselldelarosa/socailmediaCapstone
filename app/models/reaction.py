4c6066b386618252c52a3df3e82afc9310a1624b
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2023-03-09 19:09:35 -0500

    starting reaction routes

---
+++ app/models/reaction.py
@@ -0,0 +1,7 @@
+# from .db import db, environment, add_prefix_for_prod
+# from sqlalchemy.sql import func
+
+# class Reaction(db.Model):
+#      __tablename='reactions'
+
+#      if environment == 'production'
